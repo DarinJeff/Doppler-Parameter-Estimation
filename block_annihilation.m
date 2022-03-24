0f0f49214c526fdd2385d2ba5b8b2e2f469eb8b4
DarinJeff <darinjeff@gmail.com>
2022-03-25 00:29:07 +0530

    block_annihilation finishing touches

--- block_annihilation.m
+++ block_annihilation.m
@@ -4,15 +4,21 @@ clc;
 % Change values in user input section, final delay estimates in Tq
 %% User Input
 % Number of samples, sampling frequency, targets, duty ratio of pulse
-N = 2000;
-fs = 1000;
-L = 3;
+N = 4000;
+fs = 2000;
+L = 4;
 D = 0.1;
 %--------------------------------------------------------------------------
-al = [0.4 0.5, 0.7];
-tl = [0.01 0.015, 0.03];
-vl = [0.04  0.07, 0.02];
+al = [0.4, 0.5, 0.7, 0.2];
+tl = [0.05, 0.1, 0.125, 0.012];
+vl = [0.04, 0.07, 0.09, 0.03];
 %% Generating Transmitted and received pulses
+%------------------------------------------
+% to ensure that the divisions are proper
+if(D*N/(4*L) ~= fix(D*N/(4*L)))
+    error('Enter D such that N*D/(4*L) is an integer')
+end
+%------------------------------------------
 P = 2*L;
 Ts = 1/fs;
 tau = 1/P * N * Ts;
@@ -50,32 +56,37 @@ subplot(3,2,2);
 plot(t,angle(s));
 title('phase input');
 subplot(3,2,3);
-plot(t,abs(x));
-title('received signal');
-ylabel('x(t)')
-xlabel('t (in sec)')
-subplot(3,2,4);
-plot(t,angle(x));
-title('phase received');
-ylabel('angle s(t)')
-xlabel('t (in sec)')
-subplot(3,2,5);
 plot(t,abs(xr));
 title('approximated received signal');
 ylabel('xr(t)');
 xlabel('t (in sec)');
-subplot(3,2,6);
+subplot(3,2,4);
 plot(t,angle(xr));
 title('approximated received signal phase');
 ylabel('angle xr(t)');
 xlabel('t (in sec)');
-%-------------------------------------------------------------------------   
+%-------------------------------------------------------------------------  
+% noise
+sigmaN = 0.000;
+xr = xr + randn(1,N)*sigmaN;
+%-------------------------------------------------------------------------  
+subplot(3,2,5);
+plot(t,abs(xr));
+title('approximated noisy received signal');
+ylabel('xr(t)');
+xlabel('t (in sec)');
+subplot(3,2,6);
+plot(t,angle(xr));
+title('approximated noisy received signal phase');
+ylabel('angle xr(t)');
+xlabel('t (in sec)');
+%-------------------------------------------------------------------------  
 % Get the P reflected pulses individually
-n = fix(N/P);
+n = N/P;
 xp = zeros(P,n);
 xpw = zeros(P,n);
 for p = 0:P-1
-   xp(p+1,:) = xr(p*n + 1: (p+1)*n);
+   xp(p+1,:) = xr(p*n + 1: (p+1)*n);   
    xpw(p+1,:) = fft(xp(p+1,:));
 end
 
@@ -114,7 +125,7 @@ xlabel('w (in rad/s)');
 K = 2*L + 1;
 xpwk = zeros(P,K);
 hwk = zeros(1,K);
-Tq = zeros(min( fix(n*Ts/max(tl)), fix(n/K)), l);
+Tq = zeros(min( fix(n*Ts/max(tl)), fix(n/K) ), l);
 
 for wo = 1: min( fix(n*Ts/max(tl)), fix(n/K))
     good_wo = true;   
@@ -137,21 +148,20 @@ for wo = 1: min( fix(n*Ts/max(tl)), fix(n/K))
        continue;
     end
     
-    F = zeros(L+1, L+1);
-    for i = 1:L+1
-        for j = 1:L+1
-            F(i,j) = zpwk(1, L+1+i-j);
+    F = zeros((L+1)*P, L+1);
+    for p = 1:P
+        for i = 1:L+1
+            for j = 1:L+1
+                F(i + (p-1)*(L+1),j) = zpwk(p, L+1+i-j);
+            end
         end
     end
 
     % annihilating filter  
 
-    if(rank(F)==L+1) 
-        disp('Rank error');
-        continue;
-    end
-    
-    A = null(F);
+
+    % eigenvector with smallest eigenvalue
+    [A,D] = eigs(F'*F,1,'SM');
     zqr = roots(A);
     
     % Ensuring that the angle is from 0 to -2pi
@@ -160,3 +170,7 @@ for wo = 1: min( fix(n*Ts/max(tl)), fix(n/K))
     
     Tq(wo, :) = tq;
 end
+% Best estimate is in last non zero row - wo
+row_is_zero = all(Tq~=0,2);
+wo = find(row_is_zero,1,'last');
+tq = Tq(wo, :)
